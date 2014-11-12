2429d36aaad7db4e9bf256ca6c896da43c42fc0f
Andreas Bäcklund <andreasb@activesolution.se>
2014-11-12 23:50:57 +0100

    Separate nick message from chat message

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * Created by andbackl on 12/11/14.
  */
 
-
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -16,10 +15,24 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 
-    io.emit('user connected', ":: User connected");
+    var nick = null;
+
+    socket.on('nick', function (nickname) {
+
+        var oldnick = nick;
+        nick = nickname;
+        console.log("nick changed");
+        if (oldnick != null)
+            io.emit('chat message', "Nick changed: " + oldnick + " is now " + nick);
+        else
+            io.emit('chat message', "New user join chat " + nick);
+
+    })
 
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        if (nick == null)
+            return;
+        io.emit('chat message', nick + ": " + msg);
     });
 });
 
